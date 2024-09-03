a3c87daccee05f99ce4fd5b0b6a6e15789510595
Benjamin Helyer <benjaminkhelyer@gmail.com>
2024-09-03 15:43:28 -0500

    Added initial code to __init__ of Simulator class and generate_data func of Simulator class.

--- src/simulation/Simulator.py
+++ src/simulation/Simulator.py
@@ -1,5 +1,9 @@
 """Methods for generating data based on given differential equations."""
 
 def Simulator():
-    def __init__():
-        ...
+    def __init__(self):
+        self.diffeq = "something"
+        self.noise = "another_thing"
+    
+    def generate_data(self, num_points: int) -> List[float]:
+        return [0.0]
