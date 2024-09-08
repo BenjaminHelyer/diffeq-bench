ab6c492e5c95fc3cb81e3c627bcb219f8c575a14
Benjamin Helyer <benjaminkhelyer@gmail.com>
2024-09-08 15:34:19 -0500

    Refactored tests to use common fixture for logistic growth parameters.

--- tests/test_simulation/test_simulator.py
+++ tests/test_simulation/test_simulator.py
@@ -12,42 +12,54 @@ sys.path.append(SRC_PATH)
 from simulation.simulator import Simulator
 
 
-def diffeq_logistic_growth(t, z, args):
-    """
-    Example diffeq for logistic growth.
-    """
-    a_x, b_x, a_y, b_y = args
-    x, y = z
-    dxdt = a_x * (b_x - x)
-    dydt = a_y * (b_y - y)
-    return [dxdt, dydt]
+@pytest.fixture
+def diffeq_logistic_growth():
+    def _diffeq_logistic_growth(t, z, args):
+        """
+        Example diffeq for logistic growth.
+        """
+        a_x, b_x, a_y, b_y = args
+        x, y = z
+        dxdt = a_x * (b_x - x)
+        dydt = a_y * (b_y - y)
+        return [dxdt, dydt]
+
+    return _diffeq_logistic_growth
 
 
-@pytest.mark.parametrize("backend_option", [("scipy"), ("pytorch"), ("jax")])
-def test_simulator_logistic_growth_happy_path(backend_option):
+@pytest.fixture
+def logistic_growth_params():
+    return (0.5, 200, 0.02, 100)
+
+
+@pytest.mark.parametrize("backend_option", ["scipy", "pytorch", "jax"])
+def test_simulator_logistic_growth_happy_path(
+    backend_option, diffeq_logistic_growth, logistic_growth_params
+):
     """
     Happy-path for running the Simulator object on a logistic
     growth model with various backends.
     """
-    a_x = 0.5
-    b_x = 200
-    a_y = 0.02
-    b_y = 100
+    ic = [1.0, 2.0]
+    ti = 0.0
+    tf = 10.0
+    dt = 0.1
 
     uut_solver = Simulator(
         backend=backend_option,
     )
+
     sol = uut_solver.generate_numeric_sol_ivp(
         diffeq_func=diffeq_logistic_growth,
-        args=(0.5, 200, 0.02, 100),
-        ic=[1.0, 2.0],
-        ti=0.0,
-        tf=10.0,
-        dt=0.1,
+        args=logistic_growth_params,
+        ic=ic,
+        ti=ti,
+        tf=tf,
+        dt=dt,
     )
 
     if backend_option == "scipy":
-        assert sol.success == True
+        assert sol.success is True
     elif backend_option == "pytorch":
         assert sol.size() == torch.Size([100, 2])
     elif backend_option == "jax":
@@ -58,18 +70,15 @@ def test_simulator_logistic_growth_happy_path(backend_option):
 
 
 @pytest.mark.parametrize("backend_option", [("scipy"), ("pytorch"), ("jax")])
-def test_simulator_cpu_sequential_solve_ics_logistic_growth(backend_option):
+def test_simulator_cpu_sequential_solve_ics_logistic_growth(
+    backend_option, diffeq_logistic_growth, logistic_growth_params
+):
     """
     Tests the simulator's benchmarking capabilities
     for a benchmarking a set of initial conditions.
 
     This is the test for the sequential CPU benchmarks.
     """
-    a_x = 0.5
-    b_x = 200
-    a_y = 0.02
-    b_y = 100
-
     ics = [[1.0, 2.0], [2.0, 2.0], [1.0, 1.0]]
 
     uut_solver = Simulator(
@@ -77,7 +86,7 @@ def test_simulator_cpu_sequential_solve_ics_logistic_growth(backend_option):
     )
     uut_solver.cpu_sequential_solve_ics(
         diffeq_func=diffeq_logistic_growth,
-        args=(0.5, 200, 0.02, 100),
+        args=logistic_growth_params,
         ics=ics,
         ti=0.0,
         tf=10.0,
@@ -95,18 +104,17 @@ def test_simulator_cpu_sequential_solve_ics_logistic_growth(backend_option):
             assert sol.ys[0].size == 100
             assert sol.ys[1].size == 100
 
+
 @pytest.mark.parametrize("backend_option", [("scipy"), ("pytorch"), ("jax")])
-def test_simulator_cpu_parallel_solve_ics_logistic_growth(backend_option):
+def test_simulator_cpu_parallel_solve_ics_logistic_growth(
+    backend_option, diffeq_logistic_growth, logistic_growth_params
+):
     """
     Tests the simulator's benchmarking capabilities
     for a benchmarking a set of initial conditions.
 
     This is the test for the parallel CPU benchmarks.
     """
-    a_x = 0.5
-    b_x = 200
-    a_y = 0.02
-    b_y = 100
 
     ics = [[1.0, 2.0], [2.0, 2.0], [1.0, 1.0]]
 
@@ -115,7 +123,7 @@ def test_simulator_cpu_parallel_solve_ics_logistic_growth(backend_option):
     )
     uut_solver.cpu_parallel_solve_ics(
         diffeq_func=diffeq_logistic_growth,
-        args=(0.5, 200, 0.02, 100),
+        args=logistic_growth_params,
         ics=ics,
         ti=0.0,
         tf=10.0,
